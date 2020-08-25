629b0f50f555b9b009602169c910d2fa9c49d4ee
Rabbi50 <jasrabbi50@gmail.com>
2020-08-25 16:44:03 +0600

    create tweets list view

--- tweeter/urls.py
+++ tweeter/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from tweets.views import home_view,tweet_details_view,tweet_list_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home_view),
+    path('tweets/',tweet_list_view),
+    path('tweetdetails/<int:tweet_id>',tweet_details_view)
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404,JsonResponse
+
+from .models import Tweet
 
 # Create your views here.
+
+def home_view(request,*args,**kwargs):
+    #return HttpResponse(f'<h1>Hello World {kwargs}<h1>')
+    return render(request,'pages/home.html',context={},status=200)
+
+
+def tweet_list_view(request,*args,**kwargs):
+    qs=Tweet.objects.all()
+    tweet_list=[{'id':x.id,'content':x.content} for x is qs]
+    data={
+        'response':tweet_list
+    }
+
+    return JsonResponse(data)
+
+def tweet_details_view(request,tweet_id,*args,**kwargs):
+
+    data={
+        'id':tweet_id
+    }
+    status=200
+    try:
+        obj=Tweet.objects.get(id=tweet_id)
+        data['content']=obj.content
+    except :
+        data['message']='Not found!'
+        status=404
+    return JsonResponse(data,status=status)
