1b7a71196db16167fce11775ccd63cec90fd64e6
Rabbi50 <jasrabbi50@gmail.com>
2020-08-25 14:43:53 +0600

    create Tweet model

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Tweet(models.Model):
+    content=models.TextField(blank=True,null=True)
+    image=models.FileField(upload_to='images/',blank=True,null=True)
