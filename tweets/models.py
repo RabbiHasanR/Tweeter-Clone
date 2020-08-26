003b0cbd58b1013f30d243225ec1028a12a80557
Rabbi50 <jasrabbi50@gmail.com>
2020-08-26 17:07:39 +0600

    new create show in first show tweet ordering desc

--- tweets/models.py
+++ tweets/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+import random
 
 # Create your models here.
 
 class Tweet(models.Model):
     content=models.TextField(blank=True,null=True)
     image=models.FileField(upload_to='images/',blank=True,null=True)
+
+    class Meta:
+        ordering=['-id']
+    def serialize(self):
+        return{
+            'id':self.id,
+            'content':self.content,
+            'likes':random.randint(0,200)
+        }
--- tweets/views.py
+++ tweets/views.py
@@ -25,7 +25,7 @@ def tweet_create_view(request,*args,**kwargs):
         print('content:',obj.content)
         obj.save()
         if request.is_ajax():
-            return JsonResponse({},status=201) #201 for created items
+            return JsonResponse(obj.serialize(),status=201) #201 for created items
         if next_url !=None and is_safe_url(next_url,ALLOWED_HOSTS):
             return redirect(next_url)
         form=TweetForm()
@@ -38,7 +38,7 @@ def tweet_list_view(request,*args,**kwargs):
     return json data
     '''
     qs=Tweet.objects.all()
-    tweet_list=[{'id':x.id,'content':x.content,'likes':random.randint(0,1237)} for x in qs]
+    tweet_list=[x.serialize() for x in qs]
     data={
         'isUser':False,
         'response':tweet_list
