013621963a4c5ee515dcb81fcb38f6079b8ce6e3
Rabbi50 <jasrabbi50@gmail.com>
2020-08-26 17:50:21 +0600

    use dajngo default Auth user model

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.conf import settings
 import random
 
 # Create your models here.
+User=settings.AUTH_USER_MODEL
 
 class Tweet(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     content=models.TextField(blank=True,null=True)
     image=models.FileField(upload_to='images/',blank=True,null=True)
 
