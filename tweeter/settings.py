f1c75a6d67c3bba0fd7259a3e25e2a8aa63163a5
Rabbi50 <jasrabbi50@gmail.com>
2020-09-06 20:20:13 +0600

    install django-cors-headers

--- tweeter/settings.py
+++ tweeter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y5a)(zdi^@ljhh6=^sxl1=$(y&9)8et556jefq$m2j=*f=bim('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 LOGIN_URL='/login'
 MAX_TWEET_LENGTH=240
 TWEET_ACTION_OPTIONS=['like','unlike','retweet']
